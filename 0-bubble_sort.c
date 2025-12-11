789aa5b1cd5fbaa4a0c440fb1e3108c7a2a49f57
Bryan <bryanmartel.po@gmial.com>
2025-12-11 16:54:49 +0000

    fix Correct output - case: array of 20 lements ordered randomly

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,17 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
+ * bubble_sort - sorts an array of integers in ascending order
  *               using the Bubble sort algorithm
- * @array: Pointer to the array to sort
- * @size: Number of elements in the array
+ * @array: pointer to the array of integers
+ * @size: number of elements in the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
-	int swapped;
+	int temp, swapped;
 
 	if (array == NULL || size < 2)
 		return;
@@ -19,18 +20,19 @@ void bubble_sort(int *array, size_t size)
 	{
 		swapped = 0;
 
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+
+				print_array(array, size);
 				swapped = 1;
 			}
 		}
 
-		/* If no elements were swapped, the array is already sorted */
 		if (swapped == 0)
 			break;
 	}
